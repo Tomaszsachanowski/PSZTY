8b73706875869c2d99d6b514882e90d5a921665d
Tomaszsachanowski <tomaszsachanowski@gmail.com>
2019-03-28 19:18:57 +0100

    porzadki

--- populacja.py
+++ populacja.py
@@ -7,19 +7,23 @@ author:
 from osobnik import Osobnik
 import numpy as np
 from random import sample
+from random import random
 
 
 class Populacja:
 
-    def __init__(self, lam, mi, data=None):
+    def __init__(self, lam, mi, pm, data=None):
         """
         lam- liczba generowanych potomkow
         mi- liczba osobnikow w każdym pokoleniu
+        pm - prawdopodobienstwo mutacji
         """
         self.data = data
         self.wymiar_d = data.shape[1]
         self.lam = lam
         self.mi = mi
+        # pm trzeba podac jako 0.*
+        self.pm = pm
 
         self.populacja_P = [
             Osobnik(wektor_wspol_w=wektor, data=data)
@@ -36,9 +40,20 @@ class Populacja:
             rodzic_A, rodzic_B = sample(population=self.populacja_P, k=2)
             wektor_sumy = (rodzic_A.wektor_wspol_w + rodzic_B.wektor_wspol_w)
             wektor_sr_arytm = wektor_sumy/2
+            # mutacja
+            nowy_wektor = self.mutacja(wektor_sr_arytm)
             # tworze nowego osobnika
-            potomek = Osobnik(wektor_wspol_w=wektor_sr_arytm, data=self.data)
+            potomek = Osobnik(wektor_wspol_w=nowy_wektor, data=self.data)
             self.populacja_potomkow.append(potomek)
+
+    # Ta funkcja musi byc uruchamiana przez krzyzowanie dla kazdego nowego potomka
+    def mutacja(self, wektor_wspol):
+        if random() < self.pm:
+            # tworze wektor o takiej samej wielkosci jak wektor wspolczynnikow
+            # i umieszczam w nim wylosowane prawdopodobienstwa
+            wektor_mutacji = np.random.normal(loc=1.0, scale=0.3, size=wektor_wspol.shape[0])
+            wektor_wspol = wektor_wspol*wektor_mutacji
+        return wektor_wspol
 
     def selekcja_loss_1(self):
         tmp = self.populacja_P + self.populacja_potomkow
